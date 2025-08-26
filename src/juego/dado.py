452eb18dc747e32bbf0c28d2a8df6bfb703c3644
Jhonweedmann <benjaminjr2004@gmail.com>
2025-08-26 18:12:50 -0400

    GREEN: Implementacion minima para numero al azar

---
+++ src/juego/dado.py
@@ -0,0 +1,5 @@
+import random
+
+class Dado:
+    def lanzar(self):
+        return random.randint(1, 6)
--- tests/test_dado.py
+++ tests/test_dado.py
@@ -1,7 +1,9 @@
 import pytest
+from src.juego.dado import Dado
 
 def test_dado_devuelve_numero_entre_1_y_6():
-    from dado import Dado   # aún no existe dado.py
     d = Dado()
     resultado = d.lanzar()
     assert 1 <= resultado <= 6
+
+
