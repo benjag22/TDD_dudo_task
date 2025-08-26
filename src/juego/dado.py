69eeab786b99a305098beb37462c2115a744ed15
Jhonweedmann <benjaminjr2004@gmail.com>
2025-08-26 19:02:37 -0400

    RED: Test para definir el valor del dado - falla como esperado

--- src/juego/dado.py
+++ src/juego/dado.py
@@ -3,3 +3,6 @@ import random
 class Dado:
     def lanzar(self):
         return random.randint(1, 6)
+
+
+#1: "As", 2: "Tonto", 3: "Tren", 4: "Cuadra", 5: "Quina", 6: "Sexto"
--- tests/test_dado.py
+++ tests/test_dado.py
@@ -7,3 +7,17 @@ def test_dado_devuelve_numero_entre_1_y_6():
     assert 1 <= resultado <= 6
 
 
+def test_dado_define_su_valor():
+    d = Dado()
+    resultado = d.definir(1)
+    assert resultado == "As"
+    resultado = d.definir(2)
+    assert resultado == "Tonto"
+    resultado = d.definir(3)
+    assert resultado == "Tren"
+    resultado = d.definir(4)
+    assert resultado == "Cuadra"
+    resultado = d.definir(5)
+    assert resultado == "Quina"
+    resultado = d.definir(6)
+    assert resultado == "Sexto"
