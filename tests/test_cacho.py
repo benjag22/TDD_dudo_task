95ef782e9142783fd88f1c7c9228dc0d7cb3a080
benjag22 <benjagonzalez2022@udec.cl>
2025-08-27 18:04:46 -0400

    RED: test para la funcionalidad de quitar un dado del cacho- falla como esperado

--- tests/test_cacho.py
+++ tests/test_cacho.py
@@ -10,3 +10,9 @@ def test_cacho_lanza_todos_los_dados():
     cacho = Cacho()
     resultados = cacho.lanzar_dados()
     assert all(1 <= resultado <= 6 for resultado in resultados)
+
+def test_validar_cantidad_dados_al_quitar():
+    cacho = Cacho()
+    n_dados = cacho.obtener_cantidad_de_dados()
+    resultado = cacho.quitar_dado()
+    assert (n_dados - 1 == resultado) and (resultado >= 0)
