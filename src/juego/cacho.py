cd19a65489af47d82bbc7a7d57d553d8f3436600
benjag22 <benjagonzalez2022@udec.cl>
2025-08-27 18:10:33 -0400

    GREEN: implementacion para quitar un dado del cacho

--- src/juego/cacho.py
+++ src/juego/cacho.py
@@ -1,7 +1,16 @@
 from src.juego.dado import Dado
 
 class Cacho:
-    def __init__(self):
+
+    def __init__(self) -> None:
         self.dados = [Dado() for _ in range(5)]
-    def lanzar_dados(self):
+
+    def lanzar_dados(self) -> list[int]:
         return [dado.lanzar() for dado in self.dados]
+
+    def obtener_cantidad_de_dados(self) -> int:
+        return len(self.dados)
+
+    def quitar_dado(self) -> int:
+        self.dados.pop()
+        return self.obtener_cantidad_de_dados()
