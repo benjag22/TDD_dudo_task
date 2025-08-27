35825d67809b0ce82e6b589657d1fa62057361f9
benjag22 <benjagonzalez2022@udec.cl>
2025-08-27 13:29:44 -0400

    RED: test para contar pintas con ases como comodin - falla como esperado

--- tests/test_contador_pintas.py
+++ tests/test_contador_pintas.py
@@ -1,5 +1,10 @@
 import pytest
 from src.juego.contador_pintas import ContadorPintas
+
 def test_contar_apariciones():
     contador = ContadorPintas()
     assert contador.contar_apariciones([1, 4, 6, 1, 3], 4) == 1
+
+def test_contar_apaciones_con_comodines():
+    contador = ContadorPintas()
+    assert contador.contar_apariciones_con_ases([1, 4, 6, 1, 3], 4) == 3
