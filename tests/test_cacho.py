f77e7371d0859ae8f8d078a2aec51b76a52ee2bf
Jhonweedmann <benjaminjr2004@gmail.com>
2025-08-27 01:29:53 -0400

    RED: Test para regenerar los valores - falla como esperado

--- tests/test_cacho.py
+++ tests/test_cacho.py
@@ -6,3 +6,7 @@ def test_cacho_contiene_5_dados():
     cacho = Cacho()
     assert len(cacho.dados) == 5
 
+def test_cacho_lanza_todos_los_dados():
+    cacho = Cacho()
+    resultados = cacho.lanzar_dados()
+    assert all(1 <= resultado <= 6 for resultado in resultados)
