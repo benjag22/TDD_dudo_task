2b6181b3b38ea33619185d255462ed59edf9e34d
Jhonweedmann <benjaminjr2004@gmail.com>
2025-08-27 00:30:04 -0400

    GREEN: Implementacion minima para las definiciones de Dado

--- src/juego/dado.py
+++ src/juego/dado.py
@@ -3,6 +3,19 @@ import random
 class Dado:
     def lanzar(self):
         return random.randint(1, 6)
-
+    def definir(self, valor):
+        match (valor):
+            case 1:
+                return "As"
+            case 2:
+                return "Tonto"
+            case 3:
+                return "Tren"
+            case 4:
+                return "Cuadra"
+            case 5:
+                return "Quina"
+            case 6:
+                return "Sexto"
 
 #1: "As", 2: "Tonto", 3: "Tren", 4: "Cuadra", 5: "Quina", 6: "Sexto"
