eff691be8a2427b117ddddfa6065e2fe30c19dfa
Jhonweedmann <benjaminjr2004@gmail.com>
2025-08-27 01:38:36 -0400

    GREEN: Implementacion minima para regenerar los valores de los dados en el cacho

--- src/juego/cacho.py
+++ src/juego/cacho.py
@@ -3,3 +3,5 @@ from src.juego.dado import Dado
 class Cacho:
     def __init__(self):
         self.dados = [Dado() for _ in range(5)]
+    def lanzar_dados(self):
+        return [dado.lanzar() for dado in self.dados]
