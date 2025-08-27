50db8d533551a78b61ae224552756ebbea8075f0
benjag22 <benjagonzalez2022@udec.cl>
2025-08-27 13:38:20 -0400

    GREEN:  Implementación mínima para contar pintas con ases como comodines

--- src/juego/contador_pintas.py
+++ src/juego/contador_pintas.py
@@ -2,3 +2,6 @@ class ContadorPintas:
 
     def contar_apariciones(self, dados: list[int], pinta: int) -> int:
         return dados.count(pinta)
+
+    def contar_apariciones_con_ases(self, dados: list[int], pinta: int) -> int:
+        return dados.count(pinta) + dados.count(1)
