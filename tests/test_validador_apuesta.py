97f7a5cc7bfdc66c16887f53a4bdd18fac536268
benjag22 <benjagonzalez2022@udec.cl>
2025-08-29 01:44:39 -0400

    RED: test para manejar la apuesta de otra pinta a ases - falla como esperado

--- tests/test_validador_apuesta.py
+++ tests/test_validador_apuesta.py
@@ -8,3 +8,13 @@ def test_primera_apuesta_permitida_sin_ases():
     dados_restantes = 5
     assert validador.validar_apuesta(nueva_cantidad, nueva_pinta, dados_restantes) is False
 
+def test_conversion_a_ases_cantidad_par():
+    validador = ValidadorApuesta()
+    nueva_cantidad = 6
+    nueva_pinta = 4
+    dados_restantes = 5
+    assert validador.validar_apuesta(nueva_cantidad, nueva_pinta, dados_restantes) == True
+    nueva_cantidad = (nueva_cantidad // 2) + 1
+    nueva_pinta = 1
+    dados_restantes = 5
+    assert validador.validar_apuesta(nueva_cantidad, nueva_pinta, dados_restantes) == True
