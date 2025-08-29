8d5845f56da9182513f4cbe69432aac07b75c1fb
benjag22 <benjagonzalez2022@udec.cl>
2025-08-29 02:45:20 -0400

    GREEN: implementacion para verificar si una apuesta de cambiar desde ases a otra pinta

--- src/juego/validador_apuesta.py
+++ src/juego/validador_apuesta.py
@@ -25,11 +25,28 @@ class ValidadorApuesta:
             cantidad_minima = self._convertir_a_ases(self.cantidad_actual)
             es_valida = nueva_cantidad >= cantidad_minima
 
+        elif self.pinta_actual == 1 and nueva_pinta != 1:
+            cantidad_minima = self._convertir_desde_ases(self.cantidad_actual)
+            es_valida = nueva_cantidad >= cantidad_minima
+
+        else:
+            if nueva_cantidad > self.cantidad_actual:
+                es_valida = True
+            elif nueva_cantidad == self.cantidad_actual and nueva_pinta > self.pinta_actual:
+                es_valida = True
+
         if es_valida:
             self.cantidad_actual = nueva_cantidad
             self.pinta_actual = nueva_pinta
 
         return es_valida
 
+
     def _convertir_a_ases(self, cantidad: int) -> int:
         return (cantidad // 2) + 1 if cantidad % 2 == 0 else math.ceil(cantidad / 2)
+
+
+    def _convertir_desde_ases(self, cantidad: int) -> int:
+        return (cantidad * 2) + 1
+
+
