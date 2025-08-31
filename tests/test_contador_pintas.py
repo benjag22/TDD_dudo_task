07c25050a5392223168d88afb47655bee3cf8fba
benjag22 <benjagonzalez2022@udec.cl>
2025-08-30 23:44:11 -0400

    RED: test para contar todos los dados por pinta - falla como esperado
    incluido el caso de ases como comodin

--- tests/test_contador_pintas.py
+++ tests/test_contador_pintas.py
@@ -12,3 +12,19 @@ def test_contar_apaciones_con_comodines():
 def test_contar_apaciones_con_comodines_con_un_dado():
     contador = ContadorPintas()
     assert contador.contar_apariciones_con_ases([1], 1) == 1
+
+def test_contar_todos_los_cachos():
+    contador = ContadorPintas()
+    usar_ases_como_comidines = True
+
+    cachos = [
+        [1, 2, 2, 4, 5], # pinta 1: 2 si ases = true y 1 si ases = false
+        [3, 2],
+        [1] # pinta 1: 1 siempre
+    ]
+
+    assert contador.contar_en_todos_los_dados(cachos, 1, usar_ases_como_comidines) == 3
+
+    usar_ases_como_comidines = False
+
+    assert contador.contar_en_todos_los_dados(cachos, 1, usar_ases_como_comidines) == 2
