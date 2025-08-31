392591b1ba24e55b0be82b03cac421e3e1de5109
benjag22 <benjagonzalez2022@udec.cl>
2025-08-31 00:54:26 -0400

    Se agrega tipado como clases y enums

---
+++ src/utils/dudo_types.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from enum import Enum
+
+class TipoResultado(Enum):
+    DUDADOR = "dudador"
+    APOSTADOR = "apostador"
+
+
+@dataclass
+class Apuesta:
+    cantidad: int
+    pinta: int
+
+
+@dataclass
+class ResultadoDudo:
+    apuesta_es_cierta: bool
+    quien_pierde: TipoResultado
+    dados_reales: int
+    dados_apostados: int
+
+
+@dataclass
+class ResultadoCalzar:
+    calce_exitoso: bool
+    quien_gana_dado: str | None
+    dados_reales: int
+    dados_apostados: int
