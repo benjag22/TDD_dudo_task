1802e5fe2c015a98c5f33a9e6a8e26b2ce4a7a2e
benjag22 <benjagonzalez2022@udec.cl>
2025-08-30 23:15:41 -0400

    RED: test para contar pinta ases cuando solo queda un dado - falla como esperado

--- tests/test_contador_pintas.py
+++ tests/test_contador_pintas.py
@@ -8,3 +8,7 @@ def test_contar_apariciones():
 def test_contar_apaciones_con_comodines():
     contador = ContadorPintas()
     assert contador.contar_apariciones_con_ases([1, 4, 6, 1, 3], 4) == 3
+
+def test_contar_apaciones_con_comodines_con_un_dado():
+    contador = ContadorPintas()
+    assert contador.contar_apariciones_con_ases([1], 1) == 1
